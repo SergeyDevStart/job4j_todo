1f6757dc6295a60b5c252c92ec4813fb273dd6e3
SergeyDevStart <vasenev.front@gmail.com>
2024-09-03 13:07:52 +0700

    add model - 4. TODO list [#3786]

--- db/scripts/001_ddl_create_tasks_table.sql
+++ db/scripts/001_ddl_create_tasks_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS tasks
 (
    id SERIAL PRIMARY KEY,
+   title TEXT,
    description TEXT,
    created TIMESTAMP,
    done BOOLEAN
---
+++ src/main/java/ru/job4j/todo/model/Task.java
@@ -0,0 +1,23 @@
+package ru.job4j.todo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String description;
+    private LocalDateTime created = LocalDateTime.now().withNano(0);
+    private Boolean done;
+}
